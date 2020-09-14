af3c6731b7252bc872d80fa88ff53746c7fe7aeb
antony.vargas <antony.vargas@globant.com>
2020-09-14 11:34:42 -0500

    add examples of Collectors

---
+++ src/java8/feature/collectors/CollectorsExample4.java
@@ -0,0 +1,22 @@
+package java8.feature.collectors;
+
+import java8.feature.lambda.entity.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class CollectorsExample4 {
+    public static void main(String[] args) {
+        List<Product> productsList = new ArrayList<Product>();
+        //Adding Products
+        productsList.add(new Product(1,"HP Laptop",25000f));
+        productsList.add(new Product(2,"Dell Laptop",30000f));
+        productsList.add(new Product(3,"Lenevo Laptop",28000f));
+        productsList.add(new Product(4,"Sony Laptop",28000f));
+        productsList.add(new Product(5,"Apple Laptop",90000f));
+        Double average = productsList.stream()
+                .collect(Collectors.averagingDouble(p->p.getPrice()));
+        System.out.println("Average price is: "+average);
+    }
+}
